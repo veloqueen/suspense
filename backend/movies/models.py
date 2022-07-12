26ae9d9e83a9339702a087b40aa2dd15012aa54b
Andras <andrisgombos@gmail.com>
2022-07-12 10:47:19 +0200

    added first model

---
+++ backend/movies/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+
+class Movies(models.Model):
+    name = models.CharField(max_length=75)
+    director = models.CharField(max_length=75, default='tba')
+    genre = models.CharField(max_length=75, default='tba')
+    length = models.CharField(max_length=25, default='tba')
+    year = models.CharField(max_length=4, default='tba')
+    description = models.TextField(verbose_name="text", blank=True, default='tba')
+    #avatar = models.ImageField(upload_to=None, height_field=None, width_field=None, max_length=100, blank=True, null=True,)
