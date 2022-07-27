8a56565a6d69a41126d6d4d5e759242df39b9f54
Andras <andrisgombos@gmail.com>
2022-07-27 17:34:28 +0200

    moviegrid succesfully gets data from backend

--- frontend/src/components/moviegrid.js
+++ frontend/src/components/moviegrid.js
@@ -4,38 +4,40 @@ import MovieCard from './moviecard';
 const MovieGrid = ( props ) => {
 
     const [movies, setMovies] = useState([]);
+    const [errors, setErrors] = useState(false);
     
     useEffect(()=> {
 
         const moviesURL = "http://127.0.0.1:8000/movies/";
         const config = {
             method: "GET",
-/*             headers: new Headers({
-                "Content-Type": "application/json"
-            }) */
+            headers: new Headers({
+                "Content-Type": "application/json",
+            })
         }
         fetch(moviesURL,config)
             .then(data => data.json())
             .then(data => {
                 setMovies(data);
-                console.log(setMovies)
             })
+            .catch(err => setErrors(err));
     }, []);
 
     return (
-
-        <div>
-            {movies.map((movie) => {
-                <MovieCard 
-                    key={movie.id} 
-                    name={movie.name} 
-                    director={movie.director} 
-                    year={movie.year} 
-                    description={movie.descripton}
-                    >                    
-                </MovieCard>
-            })}
-        </div>
+        <>
+            <h1>Test</h1>
+            <div>
+                {movies.map((movie) => { return <MovieCard 
+                        key={movie.id} 
+                        name={movie.name} 
+                        director={movie.director} 
+                        year={movie.year} 
+                        description={movie.description}
+                        >                 
+                    </MovieCard>
+                })}
+            </div>
+        </>
     )
 }
 
